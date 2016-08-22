3c4636eccdb2f87dd413ff7126c302c387f2d7f2
Keisuke Suda <ksuda940sc@gmail.com>
2016-08-22 22:55:53 +0900

    DAOを整理する

---
+++ app/util/DBService.scala
@@ -0,0 +1,57 @@
+package util
+
+import java.sql.Timestamp
+import javax.inject.Inject
+
+import models.Tables.{DirectoriesRow, FilesRow}
+import org.joda.time.LocalDateTime
+
+class DBService @Inject()(private val db: ObjectDao) {
+  private def nowTimestamp: Timestamp = new Timestamp(new LocalDateTime().toDateTime().getMillis)
+
+  private def toDirKeyList(path: String): Seq[String] = {
+    val dirList = path.split("/")
+
+    val result = dirList.zipWithIndex.map { case (dirName, num) =>
+      val buff = new StringBuilder
+      for (i <- 0 to num) { buff.append(dirList.apply(i) + "/") }
+      buff.toString()
+    }
+    result
+  }
+
+  def addByFilepath(userId: Int, userIds: String, collegeIds:String, path: String): Boolean = {
+    val filePath = new StringBuilder
+    var dirKeyList = Seq.empty[String]
+
+    if (path.count(_ == '/') == 0) {
+      filePath.append(path)
+    } else if (path.last == '/') {
+      dirKeyList = toDirKeyList(path)
+    } else {
+      filePath.append(path)
+      dirKeyList = toDirKeyList(path.substring(0, path.lastIndexOf("/")))
+    }
+
+    if (dirKeyList.isEmpty && !db.hasObj(filePath.toString)) {
+      db.add(FilesRow(0, None, Some(userIds), Some(collegeIds), filePath.toString, filePath.toString, userId, nowTimestamp, nowTimestamp))
+    } else if (!db.hasObj(path)) {
+      dirKeyList.map { dirPath =>
+        if (!db.hasObj(dirPath)) {
+          db.add(DirectoriesRow(0, db.findParentId(dirPath), Some(userIds), Some(collegeIds), dirPath, userId, nowTimestamp, nowTimestamp))
+        } else {
+          db.getDirectory(dirPath).map(dir => db.update(DirectoriesRow(dir.id, dir.parentId, dir.userIds, dir.collegeIds, dir.name, dir.insertedBy, dir.insertedAt, nowTimestamp))).get
+        }
+      }
+      dirKeyList.foreach(result => if (result == false) return false)
+
+      if (filePath.nonEmpty && !db.hasObj(filePath.toString)) {
+        db.add(FilesRow(0, db.findParentId(filePath.toString), Some(userIds), Some(collegeIds), filePath.substring(filePath.lastIndexOf("/") + 1), filePath.toString, userId, nowTimestamp, nowTimestamp))
+      } else {
+        true
+      }
+    } else {
+      false
+    }
+  }
+}
--- app/util/ObjectDao.scala
+++ app/util/ObjectDao.scala
@@ -1,11 +1,8 @@
 package util
 
-import java.sql.Timestamp
-
 import models.Tables.{Directories, DirectoriesRow, Files, FilesRow}
 import javax.inject.Inject
 
-import org.joda.time.LocalDateTime
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.driver.JdbcProfile
 import slick.driver.MySQLDriver.api._
@@ -18,6 +15,53 @@ import scala.util.{Failure, Success}
 class ObjectDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] {
   val files       = new TableQuery(tag => new Files(tag))
   val directories = new TableQuery(tag => new Directories(tag))
+
+  private def objType(path: String): String = if (path.last == '/') "dir" else "file"
+
+  def add[T](obj: T): Boolean = {
+    val result = obj match {
+      case obj: FilesRow => db.run(files += obj)
+      case obj: DirectoriesRow => db.run(directories += obj)
+      case other => return false
+    }
+
+    Await.ready(result, Duration.Inf)
+
+    result.value.get match {
+      case Success(addResult) => true
+      case Failure(t) => false
+    }
+  }
+
+  def update[T](obj: T): Boolean = {
+    val result = obj match {
+      case obj: FilesRow => db.run(files.filter(_.id === obj.id).update(obj))
+      case obj: DirectoriesRow => db.run(directories.filter(_.id === obj.id).update(obj))
+      case other => return false
+    }
+
+    Await.ready(result, Duration.Inf)
+
+    result.value.get match {
+      case Success(updateResult) => if (updateResult == 1) true else false
+      case Failure(t) => false
+    }
+  }
+
+  def delete[T](obj: T): Boolean = {
+    val result = obj match {
+      case obj: FilesRow => db.run(files.filter(_.id === obj.id).delete)
+      case obj: DirectoriesRow => db.run(directories.filter(_.id === obj.id).delete)
+      case other => return false
+    }
+
+    Await.ready(result, Duration.Inf)
+
+    result.value.get match {
+      case Success(deleteResult) => if (deleteResult == 1) true else false
+      case Failure(t) => false
+    }
+  }
 
   def findAll: Option[Seq[ObjectProperty]] = {
     var objects = ArrayBuffer.empty[ObjectProperty]
@@ -67,10 +111,10 @@ class ObjectDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider
       case Failure(t) => None
     }
 
-    if (objects.isEmpty) None else Some(objects)
+    if (objects.nonEmpty) Some(objects) else None
   }
 
-  private def findDirId(name: String): Option[Int] = {
+  def findDirId(name: String): Option[Int] = {
     if (name == "/") return None
 
     val dirId: Future[Option[Int]] = db.run(directories.filter(_.name === name).map(_.id).result.headOption)
@@ -86,7 +130,7 @@ class ObjectDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider
     }
   }
 
-  private def findParentId(name: String): Option[Int] = {
+  def findParentId(name: String): Option[Int] = {
     if ((name.count(_ == '/') == 1 && name.last == '/') || name.count(_ == '/') == 0) return None
 
     val id = name.last == '/' match {
@@ -102,117 +146,39 @@ class ObjectDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider
     }
   }
 
-  def add[T](obj: T): Boolean = {
-    val result = obj match {
-      case obj: FilesRow => db.run(files += obj)
-      case obj: DirectoriesRow => db.run(directories += obj)
-      case other => return false
-    }
-
-    Await.ready(result, Duration.Inf)
-
-    result.value.get match {
-      case Success(addResult) => true
-      case Failure(t) => false
-    }
-  }
-
-  def update[T](obj: T): Boolean = {
-    val result = obj match {
-      case obj: FilesRow => db.run(files.filter(_.id === obj.id).update(obj))
-      case obj: DirectoriesRow => db.run(directories.filter(_.id === obj.id).update(obj))
-      case other => return false
-    }
-
-    Await.ready(result, Duration.Inf)
-
-    result.value.get match {
-      case Success(updateResult) => if (updateResult == 1) true else false
-      case Failure(t) => false
-    }
-  }
-
-  def delete[T](obj: T): Boolean = {
-    val result = obj match {
-      case obj: FilesRow => db.run(files.filter(_.id === obj.id).delete)
-      case obj: DirectoriesRow => db.run(directories.filter(_.id === obj.id).delete)
-      case other => return false
-    }
-
-    Await.ready(result, Duration.Inf)
-
-    result.value.get match {
-      case Success(deleteResult) => if (deleteResult == 1) true else false
-      case Failure(t) => false
-    }
-  }
-
-  def addByFilepath(userId: Int, userIds: String, collegeIds:String, path: String): Boolean = {
-    val filePath = new StringBuilder
-    var dirKeyList = Seq.empty[String]
-
-    if (path.count(_ == '/') == 0) {
-      filePath.append(path)
-    } else if (path.last == '/') {
-      dirKeyList = toDirKeyList(path)
-    } else {
-      filePath.append(path)
-      dirKeyList = toDirKeyList(path.substring(0, path.lastIndexOf("/")))
-    }
-
-    if (dirKeyList.isEmpty && canAdd(filePath.toString)) {
-      add(FilesRow(0, None, Some(userIds), Some(collegeIds), filePath.toString, filePath.toString, userId, nowTimestamp, nowTimestamp))
-    } else if (canAdd(path)) {
-      dirKeyList.foreach { dirPath =>
-        if (canAdd(dirPath)) {
-          add(DirectoriesRow(0, findParentId(dirPath), Some(userIds), Some(collegeIds), dirPath, userId, nowTimestamp, nowTimestamp))
-        } else {
-          val result = db.run(directories.filter(_.id === findDirId(dirPath).get).map(_.updatedAt).update(nowTimestamp))
-
-          Await.ready(result, Duration.Inf)
-
-          result.value.get match {
-            case Success(updateResult) => if (updateResult != 1) return false
-            case Failure(t) => return false
-          }
-        }
-      }
-
-      if (filePath.nonEmpty && canAdd(filePath.toString)) {
-        add(FilesRow(0, findParentId(filePath.toString), Some(userIds), Some(collegeIds), filePath.substring(filePath.lastIndexOf("/") + 1), filePath.toString, userId, nowTimestamp, nowTimestamp))
-      } else {
-        true
-      }
-    } else {
-      false
-    }
-  }
-
-  private def toDirKeyList(path: String): Seq[String] = {
-    val dirList = path.split("/")
-
-    val result = dirList.zipWithIndex.map { case (dirName, num) =>
-      val buff = new StringBuilder
-      for (i <- 0 to num) { buff.append(dirList.apply(i) + "/") }
-      buff.toString()
-    }
-    result
-  }
-
-  private def canAdd(path: String): Boolean = {
+  def hasObj(path: String): Boolean = {
     val obj = objType(path) match {
-      case "dir"  => db.run(directories.filter(_.name === path).result.headOption)
+      case "dir"  => db.run(directories.filter (_.name === path).result.headOption)
       case "file" => db.run(files.filter(_.path === path).result.headOption)
     }
 
     Await.ready(obj, Duration.Inf)
 
     obj.value.get match {
-      case Success(result) => if (result.isEmpty) true else false
+      case Success(result) => if (result.nonEmpty) true else false
       case Failure(t) => false
     }
   }
 
-  private def objType(path: String): String = if (path.last == '/') "dir" else "file"
-  private def nowTimestamp: Timestamp = new Timestamp(new LocalDateTime().toDateTime().getMillis)
+  def getDirectory(path: String): Option[DirectoriesRow] = {
+    val dir = db.run(directories.filter(_.name === path).result.headOption)
+
+    Await.ready(dir, Duration.Inf)
+
+    dir.value.get match {
+      case Success(result) => if (result.nonEmpty) result else None
+      case Failure(t) => None
+    }
+  }
+
+  def getFile(path: String): Option[FilesRow] = {
+    val file = db.run(files.filter(_.path === path).result.headOption)
+
+    Await.ready(file, Duration.Inf)
+
+    file.value.get match {
+      case Success(result) => if (result.nonEmpty) result else None
+      case Failure(t) => None
+    }
+  }
 }
