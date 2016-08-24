2b4e21e91adf1759dc60b287253eef7e1b467b5c
Keisuke Suda <ksuda940sc@gmail.com>
2016-08-25 02:34:08 +0900

    ファイルダウンロードを実装する

---
+++ app/controllers/FileController.scala
@@ -0,0 +1,25 @@
+package controllers
+
+import java.io.{File, FileOutputStream}
+import javax.inject.Inject
+import play.api.mvc.{Action, Controller}
+import util.{DBService, S3Service, Using}
+
+class FileController @Inject()(db: DBService, s3: S3Service) extends Controller {
+  def download(path: String) = Action {
+    val name: String = if (0 < path.count(_ == '/')) {
+      path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf('.'))
+    } else {
+      path.substring(0, path.lastIndexOf('.'))
+    }
+    val extension: String = path.substring(path.lastIndexOf('.'))
+    val file: File = File.createTempFile("temp", extension)
+
+    s3.download(path) match {
+      case Some(result) =>
+        for (out <- Using(new FileOutputStream(file))) out.write(result)
+        Ok.sendFile(file, fileName = {f => s"$name$extension"}, onClose = {() => file.delete})
+      case None => Status(500)
+    }
+  }
+}
