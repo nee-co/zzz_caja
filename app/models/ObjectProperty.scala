c9ec6aa4ba9ab77d3104835750a248b5280c3d24
Keisuke Suda <ksuda940sc@gmail.com>
2016-09-10 13:57:04 +0900

    overrideだった

--- app/models/ObjectProperty.scala
+++ app/models/ObjectProperty.scala
@@ -3,7 +3,7 @@ package models
 import java.sql.Timestamp
 
 case class ObjectProperty(obj_type: String, name: String, created_user: Int, created_at: Timestamp, updated_at: Timestamp) {
-  def canEqual(other: Any) = other.isInstanceOf[ObjectProperty]
+  override def canEqual(other: Any): Boolean = other.isInstanceOf[ObjectProperty]
   override def hashCode: Int = (name.hashCode + 31) * 31 + created_at.hashCode
   override def equals(other: Any) = other match {
     case that: ObjectProperty => that.canEqual(ObjectProperty.this) && obj_type == that.obj_type &&
